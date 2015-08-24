0645d0d139524b369d524ec424d551e0c7746aed
Rhadax <n.can2lon@gmail.com>
2015-08-25 00:12:51 +0200

    heroku issues 8

--- app/models/ckeditor/asset.rb
+++ app/models/ckeditor/asset.rb
@@ -1,6 +1,7 @@
 class Ckeditor::Asset < ActiveRecord::Base
   include Ckeditor::Orm::ActiveRecord::AssetBase
   include Ckeditor::Backend::Dragonfly
+  include ActiveRecord
 
   dragonfly_accessor :data, app: :ckeditor
   validates_presence_of :data
