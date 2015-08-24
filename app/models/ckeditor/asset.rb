90c08aae9bf893fdb5895aafcb72504a481fdf6c
Rhadax <n.can2lon@gmail.com>
2015-08-25 00:31:52 +0200

    remove model CKEDITOR et REDACTOR

--- app/models/ckeditor/asset.rb
+++
@@ -1,8 +0,0 @@
-class Ckeditor::Asset < ActiveRecord::Base
-  include Ckeditor::Orm::ActiveRecord::AssetBase
-  include Ckeditor::Backend::Dragonfly
-  include ActiveRecord
-
-  dragonfly_accessor :data, app: :ckeditor
-  validates_presence_of :data
-end
