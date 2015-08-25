7b2b6c89acadbaafd2a332aa7dbd6473b37b9374
Rhadax <n.can2lon@gmail.com>
2015-08-25 22:35:26 +0200

    assets issues 7

--- vendor/cache/rails_log_stdout-f9a92758f2d5/README.md
+++
@@ -1,3 +0,0 @@
-# rails_log_stdout
-
-Please use https://github.com/heroku/rails_stdout_logging instead
--- vendor/cache/rails_log_stdout-f9a92758f2d5/lib/rails_log_stdout/railtie.rb
+++
@@ -1,9 +0,0 @@
-require 'rails_log_stdout/rails3'
-
-module RailsLogStdout
-  class Railtie < ::Rails::Railtie
-    config.after_initialize do
-      Rails3.set_logger(config)
-    end
-  end
-end
